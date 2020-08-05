213715ee4f7ec6585a7495fde53856e98cc6cdf0
Chris Hesterman <chrishesterman14@gmail.com>
2020-08-05 16:59:05 -0700

    Set up basic dependiencies, scripts, configs challenge2

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoding({ extended: true }));
+app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send('Get request received');
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('listening on port 3000');
+});
