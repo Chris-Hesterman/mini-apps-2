ace7dabf957e7cb3691207f2a53cfe82d3661a1a
Chris Hesterman <chrishesterman14@gmail.com>
2020-09-22 12:04:52 -0700

    Init, install basic dependencies, get basic server up and running

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
+app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('yeah, it works');
+});
+
+app.listen(3000, () => {
+  console.log('Server up on port 3000');
+});
